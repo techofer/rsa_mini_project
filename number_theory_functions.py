4142f0638bc52775d87ec21661f2da47190ff719
Ofer Katz <ofer.katz@campus.technion.ac.il>
2022-12-16 09:09:20 +0200

    initial commit

---
+++ number_theory_functions.py
@@ -0,0 +1,101 @@
+from random import randrange
+
+def extended_gcd(a,b):
+    """
+    Returns the extended gcd of a and b
+
+    Parameters
+    ----------
+    a : Input data.
+    b : Input data.
+    Returns
+    -------
+    (d, x, y): d = gcd(a,b) = a*x + b*y
+    """
+
+
+
+def modular_inverse(a,n):
+    """
+    Returns the inverse of a modulo n if one exists
+
+    Parameters
+    ----------
+    a : Input data.
+    n : Input data.
+
+    Returns
+    -------
+    x: such that (a*x % n) == 1 and 0 <= x < n if one exists, else None
+    """
+
+
+def modular_exponent(a, d, n):
+    """
+    Returns a to the power of d modulo n
+
+    Parameters
+    ----------
+    a : The exponential's base.
+    d : The exponential's exponent.
+    n : The exponential's modulus.
+
+    Returns
+    -------
+    b: such that b == (a**d) % n
+    """
+
+def miller_rabin(n):
+    """
+    Checks the primality of n using the Miller-Rabin test
+
+    Parameters
+    ----------
+    n : The number to check
+
+    Returns
+    -------
+    b: If n is prime, b is guaranteed to be True.
+    If n is not a prime, b has a 3/4 chance at least to be False
+    """
+    a = randrange(1,n)
+    k = 0
+    d = n-1
+    while d % 2 == 0:
+        k = k + 1
+        d = d // 2
+    x = modular_exponent(a, d, n)
+    if x == 1 or x == n-1:
+        return True
+    for _ in range(k):
+        x = (x * x) % n
+        if x == 1:
+            return False
+        if x == n-1:
+            return True
+    return False
+
+def is_prime(n):
+    """
+    Checks the primality of n
+
+    Parameters
+    ----------
+    n : The number to check
+
+    Returns
+    -------
+    b: If n is prime, b is guaranteed to be True.
+    If n is not a prime, b has a chance of less than 1e-10 to be True
+    """
+    for _ in range(10):
+        if not miller_rabin(n):
+            return False
+    return True
+
+def generate_prime(digits):
+    for i in range(digits * 10):
+        n = randrange(10**(digits-1), 10**digits)
+        if is_prime(n):
+            return n
+    return None
---
+++ rsa_functions.py
@@ -0,0 +1,50 @@
+import number_theory_functions
+
+class RSA():
+    def __init__(self, public_key, private_key = None):
+        self.public_key = public_key
+        self.private_key = private_key
+
+    @staticmethod
+    def generate(digits = 10):
+        """
+        Creates an RSA encryption system object
+
+        Parameters
+        ----------
+        digits : The number of digits N should have
+
+        Returns
+        -------
+        RSA: The RSA system containing:
+        * The public key (N,e)
+        * The private key (N,d)
+        """
+
+
+    def encrypt(self, m):
+        """
+        Encrypts the plaintext m using the RSA system
+
+        Parameters
+        ----------
+        m : The plaintext to encrypt
+
+        Returns
+        -------
+        c : The encrypted ciphertext
+        """
+
+
+    def decrypt(self, c):
+        """
+        Decrypts the ciphertext c using the RSA system
+
+        Parameters
+        ----------
+        c : The ciphertext to decrypt
+
+        Returns
+        -------
+        m : The decrypted plaintext
+       """
